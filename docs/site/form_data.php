93428d013c2bcec9aa2ef32edb18563a155cf82e
ManujaH <manuja.nbh@gmail.com>
2020-11-04 19:03:54 +0530

    .csv

--- docs/site/form_data.php
+++ docs/site/form_data.php
@@ -8,7 +8,7 @@ $data=$name.",".$email;
 $file="file.csv";
 
 
-file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+file_put_contents($file, $data.PHP_EOL, FILE_APPEND);
 print "<h1 align=center>Thank you for submitting your email address!</h1>";
 
 
